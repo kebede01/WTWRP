594b3cd42618a8693417a5abe100f8b1984772c1
kebede01 <johnzeeze@yahoo.com>
2026-03-22 17:06:55 -0700

    Implement: vite, eslint and all file structures

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <div>
-      <h1>Hello Vite!</h1>
-    </div>
-  );
-}
-
-export default Header;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import "./Main.css";
+function Main() {
+  return (
+    <div className="container">
+      <p>This is your first Vite project</p>
+    
+    </div>
+  );
+}
+
+export default Main;
