faa53c81bfa6da33d480d8d8e2c531e7f05577f3
kebede01 <johnzeeze@yahoo.com>
2026-03-22 22:04:50 -0700

    Create header.jsx and header.css along with index.css root

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import "./Header.css";
+import logo from "../../assets/images/logo.svg";
+import avatar from "../../assets/images/avatar.svg";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__section header__section_left">
+ <img src={logo} alt="what to wear logo" className="header__logo" />
+      
+      <p className="header__info">
+        June 15, <span className="header__location">New York</span>
+      </p>
+      </div>
+     
+      <div className="header__section header__section_right">
+          <button type="button" className="header__button">
+        + Add clothing
+      </button>
+
+      <p className="header__user-name">Terres Tegegne</p>
+      
+      <img src={avatar} alt="Terres Tegegne" className="header__avatar" />
+</div>
+    
+    </header>
+  );
+}
