4868dbba5fe97bd012fdb205159f5b0107ce769d
kebede01 <johnzeeze@yahoo.com>
2026-03-25 20:34:58 -0700

    feat(header): implement navigation links for Home and Profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,48 @@
 import "./Header.css";
 import logo from "../../assets/images/logo.svg";
 import avatar from "../../assets/images/avatar.svg";
-
-export default function Header({ handleActiveModal, handleAddRegistration, handleLogIn, handleProfileOpen, weatherData }) {
-
+import { NavLink } from "react-router-dom";
+export default function Header({
+  handleActiveModal,
+  handleAddRegistration,
+  handleLogIn,
+  handleProfileOpen,
+  weatherData,
+}) {
   const date = new Date();
-  const formattedDate = date.toLocaleDateString('en-US', {
-  month: 'long', // "June"
-  day: 'numeric' // "15"
-});
+  const formattedDate = date.toLocaleDateString("en-US", {
+    month: "long", // "June"
+    day: "numeric", // "15"
+  });
   return (
     <header className="header">
       <div className="header__section header__section_left">
- <img src={logo} alt="what to wear logo" className="header__logo" />
-      
-      <p className="header__info">
-          {formattedDate}, <span className="header__location">{weatherData.city }</span>
-      </p>
+        <NavLink to="/" className="header-logo__navlink">
+          <img src={logo} alt="what to wear logo" className="header__logo" />
+        </NavLink>
+
+        <p className="header__info">
+          {formattedDate},{" "}
+          <span className="header__location">{weatherData.city}</span>
+        </p>
       </div>
-     
+
       <div className="header__section header__section_right">
-          <button type="button" className="header__button" onClick={handleActiveModal}>
-        + Add clothing
-      </button>
-        <button onClick={ handleAddRegistration}>Register</button>
+        <button
+          type="button"
+          className="header__button"
+          onClick={handleActiveModal}
+        >
+          + Add clothing
+        </button>
+        <button onClick={handleAddRegistration}>Register</button>
         <button onClick={handleLogIn}>Log In</button>
         <button onClick={handleProfileOpen}>Profile Update</button>
-      <p className="header__user-name">Terres Tegegne</p>
-      
-      <img src={avatar} alt="Terres Tegegne" className="header__avatar" />
-</div>
-    
+        <NavLink to="/profile" className="header__navlink">
+          <p className="header__user-name">Terres Tegegne</p>
+          <img src={avatar} alt="Terres Tegegne" className="header__avatar" />
+        </NavLink>
+      </div>
     </header>
   );
 }
