9f316068d85c5e6196c526e923a18521afdf14d3
kebede01 <johnzeeze@yahoo.com>
2026-03-26 09:03:01 -0700

    refactor: expand clickable area for ItemCard components

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,14 +1,18 @@
-
 import "./ItemCard.css";
 export default function ItemCard({ item, handlePreviewModal }) {
-
   const handleCardClick = () => {
     handlePreviewModal(item);
-  }
+  };
   return (
-    <li key={item._id} className="cards__list-item">
-      <p className="cards__img-name">{item.name}</p>
-      <img src={item.link} alt={item.name} className="cards__img" onClick={handleCardClick}/>
+    <li className="cards__list-item">
+      <div onClick={handleCardClick} style={{ cursor: "pointer" }}>
+        <p className="cards__img-name">{item.name}</p>
+        <img
+          src={item.link}
+          alt={`A photo of ${item.name}`}
+          className="cards__img"
+        />
+      </div>
     </li>
   );
 }
